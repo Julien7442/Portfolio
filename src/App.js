cf4a25c6b3c6e4357910581af25e4517161e6eff
m4d <julien.conforto@gmail.com>
2022-12-15 14:21:16 +0100

    home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './style.css';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
